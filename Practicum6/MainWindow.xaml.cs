45c406723bbd04d4448f3175c64ab2ed3e1858e6
noorve <noor.vanewijk@student.hu.nl>
2018-05-30 17:38:42 +0200

    Add project files.

---
+++ Practicum6/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practicum6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void register(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Registratie gelukt, je kunt nu inloggen!");
+        }
+
+        private void login(object sender, RoutedEventArgs e)
+        {
+            Store secondWindow = new Store();
+            secondWindow.Show();
+        }
+    }
+}
