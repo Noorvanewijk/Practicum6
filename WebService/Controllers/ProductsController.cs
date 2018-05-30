6f05a84bd95ba492829730d7348c5d9e7de0cea0
Thomas <thomaskorevaar.dev@gmail.com>
2018-05-30 20:41:19 +0200

    Webservice toegevoegd, SQL Opgezet

---
+++ WebService/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebService.Models;
+using WebService.Services;
+
+namespace WebService.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public List<ProductModel> GetAvailible()
+        {
+            return new ProductService().GetAllAvailibleProducts();
+        }
+    }
+}
